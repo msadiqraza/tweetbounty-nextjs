5e9763b4a408e5b5405f62d0c7c687d81fe52981
Muhammad Sadiq Raza <msadiqraza18@gmail.com>
2024-10-02 11:44:03 +0500

    with vervel config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	reactStrictMode: false, // Optional: disable strict mode if needed
+	output: "standalone", // Required for Vercel to handle server-side logic correctly
+	experimental: {
+		appDir: true, // If you're using the new app directory structure in Next.js
+	},
+	webpack: (config, { isServer }) => {
+		// This allows Playwright to be used in server-side API routes
+		if (isServer) {
+			config.externals.push("@playwright/test");
+		}
+
+		return config;
+	},
+};
 
 export default nextConfig;
